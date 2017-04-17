b1f17512685341cce8ecf034e955ef1d03d5b5ef
Artur Konevnikov <artik292@gmail.com>
2017-04-17 22:18:09 +0300

    Add files via upload

---
+++ lib/friends.php
@@ -0,0 +1,14 @@
+<?PHP
+
+class friends extends \atk4\data\Model {
+	public $table = 'friends';
+	
+function init() {
+	parent::init();
+	$this->addField('name', ['caption'=>'Friend Name']);
+	$this->addField('surname');
+	$this->addField('phone_number');
+	$this->addField('e-mail');
+	$this->hasMany('money');
+}
+}
---
+++ lib/money.php
@@ -0,0 +1,13 @@
+<?PHP
+class money extends \atk4\data\Model {
+	public $table = 'money';
+	
+function init() {
+	parent::init();
+	$this->addField('type',['enum'=>['+'=>'your friend lends','-'=>'you lend']]);
+	$this->addField('amount',['type'=>'money']);
+	$this->addField('date',['type'=>'date']);
+	
+	$this->hasOne('friends');
+}
+}

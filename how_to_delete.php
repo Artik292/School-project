5823feefe345195dc9d90452fb169fff4ae8bdb6
Artur Konevnikov <artik292@gmail.com>
2017-04-20 07:37:05 +0300

    Update how_to_delete.php

--- how_to_delete.php
+++ how_to_delete.php
@@ -1,7 +1,21 @@
 <?PHP
 require 'vendor/autoload.php';
+include 'init.php';
 
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=agile_education;host=localhost','root','');
+\atk4\data\Persistence_SQL('mysql:dbname=try;host=localhost','root','');
 
-$sql = mysql_query("DELETE FROM `friends` WHERE `ID` = 7");
+class friends extends \atk4\data\Model {
+	public $table = 'hope';
+	
+function init() {
+	parent::init();
+	$this->addField('id');
+	$this->addField('name');
+	$this->addField('surname');
+	$this->addField('address');
+}
+
+$m = new friends($db);
+$m->load(7);
+$m->action('delete')->execute();

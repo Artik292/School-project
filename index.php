49d8655fcf19d4c52b250e3f88ad1f85ecafbf10
Artur Konevnikov <artik292@gmail.com>
2017-04-27 16:16:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?PHP
+require 'init.php';
+
+$grid = $layout->add('CRUD');
+$grid->setModel(new friends($db));
--- init.php
+++ init.php
@@ -9,7 +9,3 @@ $db = new
 \atk4\data\Persistence_SQL('mysql:dbname=lending;host=localhost','root','');
 
 $layout->leftMenu->addItem(['VK','icon'=>'vk']);
-
-$grid = $layout->add('CRUD');
-$grid->setModel(new friends($db));
-//$grid->addAction('Loan',new \atk4\ui\jsExpression('document.location="fgfg.php";'));
